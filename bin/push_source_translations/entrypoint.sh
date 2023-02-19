164d446e41753712b39598bca709b05a88368226
Ori Hoch <ori@uumpa.com>
2023-02-19 16:28:33 +0200

    upgrade tx client CLI

--- bin/push_source_translations/entrypoint.sh
+++ bin/push_source_translations/entrypoint.sh
@@ -6,9 +6,11 @@ cd /opt/ckanext-datacity &&\
 ( cd /opt/push_source_translations/venv/src/ckan && /opt/push_source_translations/venv/bin/python setup.py extract_messages ) &&\
 msgcat /opt/push_source_translations/venv/src/ckan/ckan/i18n/ckan.pot ckanext/datacity/i18n/ckanext-datacity.pot > ckanext/datacity/i18n/ckan-datacity.pot &&\
 echo "[https://www.transifex.com]
-api_hostname = https://api.transifex.com
-hostname = https://www.transifex.com
-password = $TRANSIFEX_API_TOKEN
-username = api
+api_hostname  = https://api.transifex.com
+hostname      = https://www.transifex.com
+username      = api
+password      = $TRANSIFEX_API_TOKEN
+rest_hostname = https://rest.api.transifex.com
+token         = $TRANSIFEX_API_TOKEN
 " > ~/.transifexrc &&\
 tx push -s
